e7400f182c501f7125af50617cca30cdfaae71eb
JasonZhour <jason.zhour@gmail.com>
2019-07-29 15:21:12 +0800

    feat(init): init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './Header.scss'
+
+function Header() {
+  return (
+    <div class="header">
+      我是Header
+    </div>
+  )
+}
+
+export default Header
